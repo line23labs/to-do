e6f8c89d68279142ac45cee92e1b18a37dc72bbc
austin75stanley <austinstanleybi@gmail.com>
2019-08-01 10:30:31 +0300

    change server message

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+app.listen(port, () => console.log(`Server listening on http://127.0.0.1:${port}`));
