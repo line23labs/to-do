cd0e029139fa7f70bf2e4e1e5abd745cf70c752f
austin75stanley <austinstanleybi@gmail.com>
2019-08-02 10:40:33 +0300

    Fetch tasks

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const {Task} = require('./src/model/tasks');
 
 const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost:27017/todo';
 mongoose.connect(databaseUrl, {useNewUrlParser: true});
@@ -9,5 +10,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/api/tasks', (req, res) => {
+  Task.find({}).exec((error, tasks) => {
+    if (error) {
+      res.json({
+        error: error.message,
+      });
+    } else {
+      res.json(tasks);
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Server listening on http://127.0.0.1:${port}`));
