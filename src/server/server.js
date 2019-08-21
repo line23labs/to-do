36bd68f72466ab0f1cfcccbc3b60a36c7cfb2fa0
Prisca Nicktar <mbabaziprisca5@gmail.com>
2019-08-21 11:03:04 +0300

    remove updateRecords

--- src/server/server.js
+++ src/server/server.js
@@ -26,6 +26,5 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/api/tasks', (req, res) => new TaskManager(req, res).find());
 app.get('/api/task/:id', (req, res) => new TaskManager(req, res).findById(req.params.id));
 app.delete('/api/task/:id', (req, res) => new TaskManager(req, res).deleteTask(req.params.id));
-app.put('/api/task/:id', (req, res) => new TaskManager(req, res).updateRecords());
 
 app.listen(port, () => console.log(`Server listening on http://127.0.0.1:${port}`));
