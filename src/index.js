6ab2ad2dc05876bcafb1400936c2586ddcaaa50f
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-08-21 10:10:19 +0300

    [Fix] eslint & babel config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/**
+ * Main Application Component
+ */
 class App extends React.Component {
-    render() {
-        return <div>Welcome to React</div>;
-    }
+  /**
+   * Renders the component DOM
+   * @return {void}
+   */
+  render() {
+    return <div>Welcome to React</div>;
+  }
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
