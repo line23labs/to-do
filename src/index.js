28f219e4b34bebb3735e679b4983a70d8981a2f7
shirley <shirleymbabazi@gmail.com>
2019-09-09 13:02:54 +0300

    add missing dependencies

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Login} from './components/SignUp';
+
+/**
+ * Main Application Component
+ */
+class App extends React.Component {
+  /**
+   * Renders the component DOM
+   * @return {void}
+   */
+  render() {
+    return <div><Login></Login></div>;
+  }
+}
+ReactDOM.render(<App/>, document.getElementById('app'));
