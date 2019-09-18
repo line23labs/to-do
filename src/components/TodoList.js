3f0007bf2180214d623df3b8af4fd54609b7b492
austin75stanley <austinstanleybi@gmail.com>
2019-09-18 19:15:18 +0300

    Style.Css Addition

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import {Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 export default class TodoList extends Component {
 
   render() {
     return (
-      <div className="card">
-          Hello World
-      </div>
+
+      <div className="box box-primary"> Hello World</div>
+
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from "react";
 import TodoList from "./components/TodoList";
 import ReactDOM from "react-dom";
-import "admin-lte/dist/css/adminlte.css";
+import "admin-lte/dist/css/AdminLTE.css";
+import './components/styles/style.css'
 /**
  * Main Application Component
  */
 class App extends React.Component {
-  /**
-   * Renders the component DOM
-   * @return {void}
-   */
-
-  render() {
-
-    return (
-      <div>
-      <TodoList/>
-      </div>
-    );
-  }
+    /**
+     * Renders the component DOM
+     * @return {void}
+     */
+    render() {
+        return (
+            <div>
+                <TodoList />
+            </div>
+        );
+    }
 }
 ReactDOM.render(<App />, document.getElementById("app"));
