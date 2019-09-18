f163d265187ab4d6d2fd6e38145b37f12477a9d6
austin75stanley <austinstanleybi@gmail.com>
2019-09-18 18:31:30 +0300

    Admin LTE

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import TodoList from "./components/TodoList";
+import ReactDOM from "react-dom";
+import "admin-lte/dist/css/adminLTE.css";
+/**
+ * Main Application Component
+ */
+class App extends React.Component {
+  /**
+   * Renders the component DOM
+   * @return {void}
+   */
+
+  render() {
+
+    return (
+      <div>
+      <TodoList/>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById("app"));
